bd8d33c6f660e298fb51889de14cc7e316dd669a
Evoteks Digital Creative <evoteks.digital@gmail.com>
2025-07-15 12:25:44 +0700

    Delete db/migrations directory

--- db/migrations/20250506042308_add_cover_image_column_to_groups_table.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-ALTER TABLE groups ADD COLUMN IF NOT EXISTS cover_image VARCHAR(255);
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-ALTER TABLE groups DROP COLUMN IF EXISTS cover_image;
--- +goose StatementEnd
